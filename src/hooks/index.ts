dec734458a592de0b79d6d19c0c4a9c37432dc4b
Raman992 <ramanshakya9863@gmail.com>
2026-06-05 20:12:10 +0545

    Add exports for new hooks managing creators, campaigns, applications, messaging, deals, and notifications

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -3,3 +3,61 @@ export { useUserRole } from "./use-user-role";
 export { useToast } from "./use-toast";
 export { useDebounce } from "./use-debounce";
 export { useRealtimeMessages, useRealtimeNotifications, useRealtimeDeals } from "./use-realtime";
+export {
+  useTopCreators,
+  useCreatorsWithFilters,
+  useCreatorByUsername,
+  useCreatorProfile,
+  useUpdateCreatorProfile,
+  useUpdateSocialStats,
+  useCheckUsernameAvailability,
+} from "./use-creators";
+export {
+  useOpenCampaigns,
+  useCampaignById,
+  useBrandCampaigns,
+  useCampaigns,
+  useCampaignStats,
+  useCreateCampaign,
+  useUpdateCampaign,
+  usePublishCampaign,
+  useDeleteCampaign,
+} from "./use-campaigns";
+export {
+  useCreatorApplications,
+  useApplicationsForCampaign,
+  useApplicationStats,
+  useApplyToCampaign,
+  useUpdateApplicationStatus,
+} from "./use-applications";
+export {
+  useConversations,
+  useConversation,
+  useMessages,
+  useUnreadMessageCount,
+  useSendMessage,
+  useStartConversation,
+  useGetOrCreateConversation,
+} from "./use-messaging";
+export {
+  useDeals,
+  useDeal,
+  useActiveDeals,
+  usePendingDeals,
+  useActiveDealCount,
+  useCreateDeal,
+  useUpdateDealStatus,
+  useUpdateDealPayment,
+  useAcceptDeal,
+  useRejectDeal,
+  useCompleteDeal,
+  useInitiateEscrow,
+  useReleasePayment,
+} from "./use-deals";
+export {
+  useNotifications,
+  useUnreadNotificationCount,
+  useMarkNotificationAsRead,
+  useMarkAllNotificationsAsRead,
+  useRemoveNotification,
+} from "./use-notifications";
